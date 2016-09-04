0e2d4f7edb95ea8e670c6c3fde7920bd62c1ad34
Ayokunle Adeosun <Ayokunle Adeosun>
2016-09-04 10:27:23 +0100

    Changes

--- app/src/main/java/secured/hope/hopesecured/EnrollActivity.java
+++ app/src/main/java/secured/hope/hopesecured/EnrollActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -11,12 +12,16 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.kunall17.entryscreenmanager.Fragments.Enroll;
@@ -31,8 +36,39 @@ public class EnrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (this.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.scroll_layer);
+        ll.setMinimumHeight(screenHeight - actionBarHeight);
+        */
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        EditText edit = (EditText) findViewById(R.id.DOB);
+        edit.measure(0, 0);
+        int height = edit.getMeasuredHeight();
+
+        AppCompatButton DOB_button = (AppCompatButton)
+                findViewById(R.id.DOB_button);
+        DOB_button.setHeight(height);
+
+        EditText country = (EditText) findViewById(R.id.country);
+        country.measure(0, 0);
+        height = country.getMeasuredHeight();
+
+        AppCompatButton nextForm = (AppCompatButton)
+                findViewById(R.id.nextForm);
+        nextForm.setHeight(height);
 
     }
 
